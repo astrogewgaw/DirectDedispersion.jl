63bae282bf25a97fbce49afcd32d61f6437acfed
Ujjwal Panda <ujjwalpanda97@gmail.com>
2023-12-26 11:09:47 +0530

    🐛 Resolve a bug where signal disappears.
    This commit resolves a bug where the signal disappeared if it were very
    close to the end of the file, since then we only summed up till $N_{t} -
    \xi_{\mathrm{max}}$, where $\xi_{\mathrm{max}}$ is the dispersive delay
    across the entire band.

--- src/DirectDedispersion.jl
+++ src/DirectDedispersion.jl
@@ -1,46 +1,38 @@
 module DirectDedispersion
 
 const 𝓓 = 4.1488064239e3
-Δt(f₁, f₂, DM) = 𝓓 * DM * (f₁^-2 - f₂^-2)
+Δt(f₁, f₂, dm) = 𝓓 * dm * (f₁^-2 - f₂^-2)
 
-function Δξ!(
-    ξ;
-    f₀,
-    nf,
-    nt,
-    Δf,
-    δt,
-    DM
-)
+function Δξ!(ξ; f₀, nf, Δf, δt, dm)
     for i ∈ eachindex(ξ)
         i′ = i - 1
         f = f₀ - (i′ * Δf / nf)
-        ξᵢ = Δt(f, f₀, DM) ÷ δt
-        ξ[i] = ξᵢ < nt ? ξᵢ : 0
+        ξ[i] = Δt(f, f₀, dm) ÷ δt
     end
 end
 
-function Δξ(; f₀, nf, nt, Δf, δt, DM)
+function Δξ(; f₀, nf, Δf, δt, dm)
     ξ = zeros(Int64, nf)
-    Δξ!(ξ; f₀, nf, nt, Δf, δt, DM)
+    Δξ!(ξ; f₀, nf, Δf, δt, dm)
     ξ
 end
 
-function dd(
-    I;
-    f₀,
-    Δf,
-    δt,
-    DM
-)
+function dd!(X, I; f₀, Δf, δt, dm)
     nf, nt = size(I)
-    ξ = Δξ(; f₀, nf, nt, Δf, δt, DM)
-    X = zeros(Float64, nt - maximum(ξ))
-    @inbounds Threads.@threads for i ∈ axes(I, 1)
+    ξ = Δξ(; f₀, nf, Δf, δt, dm)
+    @inbounds for i ∈ axes(I, 1)
         @inbounds for j ∈ eachindex(X)
-            X[j] += I[CartesianIndex(i, j + ξ[i])]
+            j′ = j + ξ[i]
+            if j′ <= nt
+                X[j] += I[CartesianIndex(i, j′)]
+            end
         end
     end
+end
+
+function dd(I; f₀, Δf, δt, dm)
+    X = zeros(Float64, size(I, 2))
+    dd!(X, I; f₀, Δf, δt, dm)
     X
 end
 
