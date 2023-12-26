65a22555725ef13b36100f693c00c27898b1f568
Ujjwal Panda <ujjwalpanda97@gmail.com>
2023-12-26 11:12:38 +0530

    ✅ Add test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
-using DirectDedispersion
 using Test
+using DirectDedispersion
 
 @testset "DirectDedispersion.jl" begin
-    # Write your tests here.
+    open("./data/frb.fil") do s
+        seek(s, 362)
+        data = Array{UInt8}(read(s))
+        data = reshape(data, 128, :)
+        ts = DirectDedispersion.dd(
+            data;
+            f₀=499.21875,
+            Δf=200.0,
+            δt=1.31072e-3,
+            dm=1000.0,
+        )
+        @test argmax(ts) == 11563
+        @test maximum(ts) ≈ 4540.0
+    end
 end
