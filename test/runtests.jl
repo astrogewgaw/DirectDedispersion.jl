5ece77cdff624a9f625e5052104c8b375a14e129
Ujjwal Panda <ujjwalpanda97@gmail.com>
2024-01-01 15:16:40 +0530

    ✅ Update tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,17 +3,28 @@ using DirectDedispersion
 
 @testset "DirectDedispersion.jl" begin
     open("./data/frb.fil") do s
-        seek(s, 362)
+        seek(s, 360)
         data = Array{UInt8}(read(s))
         data = reshape(data, 128, :)
+
+        Δf = 200.0
+        dm = 1000.0
+        f₀ = 499.21875
+        δt = 1.31072e-3
+
         ts = DirectDedispersion.dd(
             data;
-            f₀=499.21875,
-            Δf=200.0,
-            δt=1.31072e-3,
-            dm=1000.0,
+            f₀=f₀,
+            Δf=Δf,
+            δt=δt,
+            dm=dm,
         )
-        @test argmax(ts) == 11563
-        @test maximum(ts) ≈ 4540.0
+
+        nt = size(ts, 1)
+        t = range(0.0, nt * δt, nt)
+
+        @test argmax(ts) == 3902
+        @test maximum(ts) ≈ 8472.0
+        @test t[argmax(ts)] ≈ 5.0 atol = 1.0
     end
 end
