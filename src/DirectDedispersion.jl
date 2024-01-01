ab843b1be493800cc56c5d61c3f61b1cbc280bf1
Ujjwal Panda <ujjwalpanda97@gmail.com>
2024-01-01 15:16:00 +0530

    🎨 Calculate delays on-the-fly.

--- src/DirectDedispersion.jl
+++ src/DirectDedispersion.jl
@@ -1,30 +1,16 @@
 module DirectDedispersion
 
 const 𝓓 = 4.1488064239e3
-Δt(f₁, f₂, dm) = 𝓓 * dm * (f₁^-2 - f₂^-2)
-
-function Δξ!(ξ; f₀, nf, Δf, δt, dm)
-    for i ∈ eachindex(ξ)
-        i′ = i - 1
-        f = f₀ - (i′ * Δf / nf)
-        ξ[i] = Δt(f, f₀, dm) ÷ δt
-    end
-end
-
-function Δξ(; f₀, nf, Δf, δt, dm)
-    ξ = zeros(Int64, nf)
-    Δξ!(ξ; f₀, nf, Δf, δt, dm)
-    ξ
-end
 
 function dd!(X, I; f₀, Δf, δt, dm)
     nf, nt = size(I)
-    ξ = Δξ(; f₀, nf, Δf, δt, dm)
-    @inbounds for i ∈ axes(I, 1)
+    for i ∈ axes(I, 1)
+        f = f₀ - ((i - 1) * Δf / nf)
+        ξ = round(Int, 𝓓 * dm * (f^-2 - f₀^-2) / δt)
         @inbounds for j ∈ eachindex(X)
-            j′ = j + ξ[i]
-            if j′ <= nt
-                X[j] += I[CartesianIndex(i, j′)]
+            @inbounds jj = j + ξ
+            if jj < nt
+                X[j] += I[CartesianIndex(i, jj)]
             end
         end
     end
