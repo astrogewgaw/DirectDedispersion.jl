a5805aaf3c5cc94390b00f25d40c982b439747e5
Ujjwal Panda <ujjwalpanda97@gmail.com>
2024-01-04 14:09:32 +0530

    🐛 Place `@inbounds` at the correct place.

--- src/DirectDedispersion.jl
+++ src/DirectDedispersion.jl
@@ -4,11 +4,11 @@ const 𝓓 = 4.1488064239e3
 
 function dd!(X, I; f₀, Δf, δt, dm)
     nf, nt = size(I)
-    for i ∈ axes(I, 1)
+    @inbounds for i ∈ axes(I, 1)
         f = f₀ - ((i - 1) * Δf / nf)
         ξ = round(Int, 𝓓 * dm * (f^-2 - f₀^-2) / δt)
         @inbounds for j ∈ eachindex(X)
-            @inbounds jj = j + ξ
+            jj = j + ξ
             if jj < nt
                 X[j] += I[CartesianIndex(i, jj)]
             end
